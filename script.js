deef05aaa8f4ce2285e0a9cd974d892c268c07d8
Lena Ksenofontova <lena.ksenofontova@mail.ru>
2022-05-31 11:16:04 +0300

    Исправлен код-стайл

--- script.js
+++ script.js
@@ -2,39 +2,40 @@
 let title = prompt('Как называется ваш проект?');
 let screens = prompt('Какие типы экранов нужно разработать?');
 let screenPrice = +prompt('Сколько будет стоить данная работа?');
-const rollBack = 20;
 let adaptive = confirm('Нужен ли адаптив на сайте?');
 let service1 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice1 = +prompt('Сколько это будет стоить?');
 let service2 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice2 = +prompt('Сколько это будет стоить?');
+const rollBack = 20;
 let fullPrice = (screenPrice + servicePrice1 + servicePrice2);
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollBack / 100)));
 
+if (fullPrice >= 30000) {
+    console.log('Даем скидку в 10%');
+} else if (fullPrice >= 15000) {
+    console.log('Даем скидку в 5%');
+} else if (fullPrice >= 0) {
+    console.log('Скидка не предусмотрена');
+} else {
+    console.log('Что то пошло не так');
+}
 
 console.log(typeof (title));
 console.log(typeof (fullPrice));
 console.log(typeof (adaptive));
-console.log(screens.length);
-console.log(`Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани`);
-console.log(`Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`);
-const loverScreens = screens.toLowerCase();
-console.log(loverScreens.split(', '));
-console.log(fullPrice * (rollBack / 100));
 
-let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollBack / 100)));
+console.log(screens.length);
 console.log(servicePercentPrice);
 
-if (fullPrice >= 30000) {
-    console.log('Даем скидку в 10%');
-}
+console.log(`Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`);
 
-else if (fullPrice >= 15000) {
-    console.log('Даем скидку в 5%');
-}
+/*
+const loverScreens = screens.toLowerCase();
+console.log(loverScreens.split(', '));
+console.log(fullPrice * (rollBack / 100)); */
 
-else if (fullPrice >= 0) {
-    console.log('Скидка не предусмотрена');
-}
-else {
-    console.log('Что то пошло не так');
-}
+
+
+
